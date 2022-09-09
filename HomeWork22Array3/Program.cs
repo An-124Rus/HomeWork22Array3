47ed44b673284a6e36c3e525b887d48c9f1e5e84
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-09-10 02:30:29 +0300

    HomeWork22Array3

---
+++ HomeWork22Array3/Program.cs
@@ -0,0 +1,60 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int minValue = 0;
+        int maxValue = 100;
+        int maxNumber;
+
+        Random random = new Random();
+
+        Console.WriteLine("Введите длину массива: ");
+        int rowLenth = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[rowLenth];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(minValue, maxValue);
+
+            Console.Write($"{array[i]}  ");
+        }
+
+        Console.WriteLine();
+
+        maxNumber = array[0];
+
+        if (maxNumber > array[1])
+        {
+            Console.Write($"{maxNumber}  ");
+        }
+        else
+        {
+            maxNumber = array[1];
+            Console.Write($"{maxNumber}  ");
+        }
+
+        for (int i = 2; i < array.Length - 1; i++)
+        {
+            if (maxNumber > array[i + 1] && maxNumber > array[i - 1])
+            {
+                maxNumber = array[i];
+                Console.Write($"{maxNumber}  ");
+            }
+            else
+            {
+                maxNumber = array[i + 1];
+            }
+        }
+
+        if (maxNumber > array[rowLenth - 1])
+        {
+            Console.Write($"{maxNumber}  ");
+        }
+        else
+        {
+            maxNumber = array[rowLenth - 1];
+            Console.Write($"{maxNumber}  ");
+        }
+    }
+}
